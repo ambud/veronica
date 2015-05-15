ba59a995ea416b16ad73ccd2681c1d1ff8ab8598
torettoff7 <torettoff7@users.noreply.github.com>
2015-05-15 19:47:49 -0400

    Code updates and starting implementation of TinkerPop Blueprints

---
+++ core/src/main/java/org/veronica/core/structures/VElement.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 Ambud Sharma
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.2
+ */
+package org.veronica.core.structures;
+
+import java.util.Iterator;
+
+import org.apache.tinkerpop.gremlin.structure.Element;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Property;
+
+/**
+ * 
+ * @author ambudsharma
+ */
+public class VElement implements Element {
+	
+	@Override
+	public Graph graph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object id() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String label() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <V> Iterator<? extends Property<V>> properties(String... arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <V> Property<V> property(String arg0, V arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
