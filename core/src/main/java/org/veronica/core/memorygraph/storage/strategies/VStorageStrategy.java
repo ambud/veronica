c76fb613b5229be423d8e877ed15ae7e122b87f6
ambud <ambud@users.noreply.github.com>
2015-05-27 19:16:29 -0400

    Moving current implementation under memory graph

--- core/src/main/java/org/veronica/core/memorygraph/storage/strategies/VStorageStrategy.java
+++ core/src/main/java/org/veronica/core/memorygraph/storage/strategies/VStorageStrategy.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.2
  */
-package org.veronica.core.storage.strategies;
+package org.veronica.core.memorygraph.storage.strategies;
 
 import org.apache.commons.configuration.Configuration;
 
